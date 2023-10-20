d966b0c1d349d2fca16c5f5db7435c562109db1a
Ekaterina <ketrin.yevstigneeva@bk.ru>
2023-10-20 08:52:10 +0500

    задание3

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const DESCRIPTIONS = [
   'Встреча с природой'
   ];
 
-  const NAMES = [
+const NAMES = [
   'Анна',
   'Дмитрий',
   'Елена',
@@ -20,7 +20,8 @@ const DESCRIPTIONS = [
   'Максим',
   ];
 
-  const MESSAGES = ['Всё отлично!',
+const MESSAGES = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
@@ -28,8 +29,8 @@ const DESCRIPTIONS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
 
-  const COUNT_PHOTO = 25;
-  const COMMENTS_MAX = 30;
+const COUNT_PHOTO = 25;
+const COMMENTS_MAX = 30;
 
   const Likes = {
   MIN:15,
@@ -37,30 +38,30 @@ const DESCRIPTIONS = [
   };
 
   const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+    const lower = Math.ceil(Math.min(a, b));
+    const upper = Math.floor(Math.max(a, b));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
   };
 
   const addComments = () => ({
-  id: getRandomInteger(0, COUNT_PHOTO),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: MESSAGES[getRandomInteger(0, MESSAGES.length -1)],
-  name: NAMES[getRandomInteger(0, NAMES.length - 1)]
+    id: getRandomInteger(0, COUNT_PHOTO),
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: MESSAGES[getRandomInteger(0, MESSAGES.length -1)],
+    name: NAMES[getRandomInteger(0, NAMES.length - 1)]
   });
 
   const addPhoto = () =>({
-  id: getRandomInteger(1, COUNT_PHOTO),
-  url: `photos/${getRandomInteger(1, COUNT_PHOTO)}.jpg`,
-  description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-  likes: getRandomInteger(Likes.MIN, Likes.MAX),
-  comments: Array.from(getRandomInteger(0 , COMMENTS_MAX), addComments)
+     id: getRandomInteger(1, COUNT_PHOTO),
+     url: `photos/${getRandomInteger(1, COUNT_PHOTO)}.jpg`,
+     description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+     likes: getRandomInteger(Likes.MIN, Likes.MAX),
+     comments: Array.from(getRandomInteger(0 , COMMENTS_MAX), addComments)
   });
 
   const getPhoto = () => {
-  const photo = Array.from({length: COUNT_PHOTO}, addPhoto);
-  return photo;
+     const photo = Array.from({length: COUNT_PHOTO}, addPhoto);
+     return photo;
   };
 
-  getPhoto();
+getPhoto();
