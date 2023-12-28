146fe21b0512e5c0bcbb0b99c148a77952b13d58
Ekaterina <ketrin.yevstigneeva@bk.ru>
2023-12-28 16:33:21 +0500

    fix

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const urls = {
   GET: 'https://29.javascript.pages.academy/kekstagram/data',
-  POST: 'https://29.javascript.pages.academy/kekstagra',
+  POST: 'https://29.javascript.pages.academy/kekstagram',
 };
 
 const sendRequest = (onSuccess, onFail, method, body) =>{
